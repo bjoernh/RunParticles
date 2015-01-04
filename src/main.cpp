8b76dd31394eb86f95e7684baf000cd682ae8928
Doug Letterman <github@renderfast.com>
2015-01-04 00:54:51 -0800

    We have to start up the Gdi before calling font stuff (in OsmLayer) or else we'll crash.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,33 @@
 //
 
 #include <QApplication>
+
 #include "GLWidget.h"
 #include "MainWindow.h"
 #include "Map.h"
 
+#ifdef Q_OS_WIN
+#define ULONG_PTR ULONG
+#include <Windows.h>
+#include <Shlobj.h>
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define LOSHORT(l)           ((SHORT)(l))
+#define HISHORT(l)           ((SHORT)(((DWORD)(l) >> 16) & 0xFFFF))
+#include <gdiplus.h>
+#undef min
+#undef max
+#pragma comment(lib, "gdiplus")
+#endif
+
 int main(int argc, char **argv)
 {
+
+#ifdef Q_OS_WIN
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiPlusToken;
+	Gdiplus::GdiplusStartup( &gdiPlusToken, &gdiplusStartupInput, NULL );
+#endif
     QApplication app(argc, argv);
     app.setApplicationName("RunParticles");
     app.setOrganizationDomain("renderfast.com");
